2e320da4e37e6c6263795ad1de89cd8ec154bc59
longhuazhao <lxz315@case.edu>
2025-07-27 15:09:12 -0400

    Add files via upload

---
+++ Exercise 1 Skeleton/RegStokes.m
@@ -0,0 +1,19 @@
+function [A] = RegStokes(x,y,epsilon)
+
+% This function should take x,y, and epsilon and
+% output the Regularized Stokeslet matrix A from Eq 12
+
+%% Input 
+% x is a 3 x 1 vector of the force location
+% y is a 3 x 1 vector of the observation point
+% epsilon is the blob function parameter
+
+%% Output
+% A is the 3 x 3 matrix in Eq 12
+
+%%% Rewrite this part of the code
+A = zeros(3,3);
+
+
+
+end
