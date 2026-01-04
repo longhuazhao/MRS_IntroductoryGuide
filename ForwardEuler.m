c2ecfea88d4d4ed3b50ed1b1ea8b408079a1f04c
longhuazhao <lxz315@case.edu>
2026-01-04 16:00:06 -0500

    Add files via upload

---
+++ ForwardEuler.m
@@ -0,0 +1,17 @@
+function y = ForwardEuler(y,u,delta_t)
+
+% This function takes in y,u and delta_t and 
+% uses Forward Euler to update y
+
+%% Input 
+% y is a 3 x 1 vector of the observation point
+% u is a 3 x 1 velocity vector at y
+% delta_t is the time step
+
+%% Output
+% y is the UPDATED position
+
+%%% Rewrite this part of the code
+y = 0;
+
+end
---
+++ MainForce.m
@@ -0,0 +1,60 @@
+clear all; close all;
+
+%Initial Positions and Force
+x = [1; -1; 2]; % forcing location
+f = [2; 3; -1]; % force
+y = [2; 0;  1];  %oberservation point
+
+%Parameters
+epsilon = 0.05;
+mu = 1;
+
+
+%Time Parameters
+t=0; %Initial Time
+tFinal=0.06; % Final Time
+delta_t=0.01; %Time Step
+N=tFinal/delta_t; %Number of Time Steps
+    
+format long
+for i=1:N
+
+    % Call the Reg Stokes Function to compute A
+    A= RegStokes(x,y,epsilon);
+   
+
+    %Use A to compute the velcoity u
+    u = 1/(8*pi*mu)*A*f;
+
+    %Update the position with Forward Euler
+    y = ForwardEuler(y,u,delta_t);
+
+
+    %Update the time
+    t = i*delta_t;
+
+
+    %Visualize the results
+    if mod(t,.01)==0
+
+        fg1=figure(1);
+
+        plot3(y(1),y(2),y(3),'b.','MarkerSize',20)
+
+        title(strcat("t=",num2str(t)))
+
+        xlim([1.9,2.1])
+        ylim([-.1,.2])
+        zlim([0.9,1.1])
+
+        xlabel('x')
+        ylabel('y')
+        zlabel('z')
+
+        pause(.01)
+
+    end
+
+end
+
+
